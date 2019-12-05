c650185d5e3f20a44f34f0b90bf319a0035592f3
Timo <dev@timot.se>
2019-12-05 22:02:40 +0100

    day 4

--- src/day4.rs
+++ src/day4.rs
@@ -1,15 +1,24 @@
 use itertools::Itertools;
 
 pub fn step1() {
-    dbg!((111110..111112)
+    let result = (245318..=765747)
         .map(|number| {
             number.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect::<Vec<u32>>()
         })
-        .find(|digits| digits.windows(2).all(is_not_decr) && digits.windows(2).any(is_same))
-        .map(|digits| digits.iter().join("")));
+        .filter(|digits| digits.windows(2).all(is_not_decr) && digits.windows(2).any(is_same))
+        .count();
+    dbg!(result);
 }
 
-pub fn step2() {}
+pub fn step2() {
+    let result = (245318..=765747)
+        .map(|number| {
+            number.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect::<Vec<u32>>()
+        })
+        .filter(|digits| digits.windows(2).all(is_not_decr) && has_grouped_same_two(digits))
+        .count();
+    dbg!(result);
+}
 
 fn is_not_decr(numbers: &[u32]) -> bool {
     match numbers {
@@ -23,4 +32,23 @@ fn is_same(numbers: &[u32]) -> bool {
         &[n1, n2] => n1 == n2,
         _ => false,
     }
+}
+
+fn has_grouped_same_two(numbers: &[u32]) -> bool {
+    numbers.iter().peekable().batching(|iter| {
+        let mut count = 1;
+        match iter.next() {
+            None => None,
+            Some(x) => {
+                while let Some(y) = iter.peek() {
+                    if y != &x {
+                        return Some(count);
+                    }
+                    iter.next();
+                    count += 1;
+                }
+                Some(count)
+            }
+        }
+    }).any(|count| count == 2)
 }
