a5258c9bd430d2f14a52b80342b23f283d875fd0
Timo <timo@mondido.com>
2019-12-06 13:03:13 +0100

    do not convert char to to number, comparing chars works

--- src/day4.rs
+++ src/day4.rs
@@ -2,9 +2,7 @@ use itertools::Itertools;
 
 pub fn step1() {
     let result = (245318..=765747)
-        .map(|number| {
-            number.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect::<Vec<u32>>()
-        })
+        .map(|number| number.to_string().chars().collect::<Vec<char>>())
         .filter(|digits| digits.windows(2).all(is_not_decr) && digits.windows(2).any(is_same))
         .count();
     dbg!(result);
@@ -12,43 +10,45 @@ pub fn step1() {
 
 pub fn step2() {
     let result = (245318..=765747)
-        .map(|number| {
-            number.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect::<Vec<u32>>()
-        })
+        .map(|number| number.to_string().chars().collect::<Vec<char>>())
         .filter(|digits| digits.windows(2).all(is_not_decr) && has_grouped_same_two(digits))
         .count();
     dbg!(result);
 }
 
-fn is_not_decr(numbers: &[u32]) -> bool {
+fn is_not_decr(numbers: &[char]) -> bool {
     match numbers {
         &[n1, n2] => n1 <= n2,
         _ => true,
     }
 }
 
-fn is_same(numbers: &[u32]) -> bool {
+fn is_same(numbers: &[char]) -> bool {
     match numbers {
         &[n1, n2] => n1 == n2,
         _ => false,
     }
 }
 
-fn has_grouped_same_two(numbers: &[u32]) -> bool {
-    numbers.iter().peekable().batching(|iter| {
-        let mut count = 1;
-        match iter.next() {
-            None => None,
-            Some(x) => {
-                while let Some(y) = iter.peek() {
-                    if y != &x {
-                        return Some(count);
+fn has_grouped_same_two(numbers: &[char]) -> bool {
+    numbers
+        .iter()
+        .peekable()
+        .batching(|iter| {
+            let mut count = 1;
+            match iter.next() {
+                None => None,
+                Some(x) => {
+                    while let Some(y) = iter.peek() {
+                        if y != &x {
+                            return Some(count);
+                        }
+                        iter.next();
+                        count += 1;
                     }
-                    iter.next();
-                    count += 1;
+                    Some(count)
                 }
-                Some(count)
             }
-        }
-    }).any(|count| count == 2)
+        })
+        .any(|count| count == 2)
 }
